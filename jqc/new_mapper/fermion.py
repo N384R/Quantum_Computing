0c4f4f48811f8c6fe4f948d3091fbb1d6db53181
N384R <juu8242@kaist.ac.kr>
2024-08-19 19:11:06 +0900

    mod

--- jqc/new_mapper/fermion.py
+++ jqc/new_mapper/fermion.py
@@ -1,3 +1,9 @@
+'''
+This module defines the Fermion class and FermionicOp class.
+'''
+
+from itertools import pairwise
+
 class Fermion:
     'Class for Fermionic operator.'
     def __init__(self, fermion):
@@ -13,7 +19,15 @@ class Fermion:
         'Return the number of the fermion.'
         if self.type == 'creation':
             return int(self.fermion.replace('^', ''))
-        return int(self.fermion)
+        return -int(self.fermion)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Fermion):
+            return self.fermion == other.fermion
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.fermion)
 
     def __repr__(self):
         sub = str.maketrans("0123456789^", "₀₁₂₃₄₅₆₇₈₉†")
@@ -21,39 +35,86 @@ class Fermion:
         line = "a" + notation
         return line
 
-class FermionString:
+class FermionicOp:
     'Class for string of Fermionic operators.'
-    def __init__(self, string):
-        self._operators = split_op(string)
+    def __init__(self, *args):
+        if isinstance(args[0], dict):
+            self._objects = args[0]
+        elif isinstance(args[0], float) and isinstance(args[1], str):
+            self._objects = split_op(args[0], args[1])
+        else:
+            raise TypeError('Invalid input.')
 
     @property
-    def operators(self):
+    def objects(self):
         'Return the operators in the string.'
-        return self._operators
+        return self._objects
 
     def __add__(self, other):
-        if isinstance(other, FermionString):
-            return FermionStrings(self, other)
-        if isinstance(other, FermionStrings):
-            return FermionStrings(self, *other)
-        return NotImplemented
+        result = self._objects.copy()
+        if isinstance(other, FermionicOp):
+            other = other._objects
+        else:
+            return NotImplemented
+        for k, v in other.items():
+            result[k] = result.get(k, 0) + v
+        return FermionicOp(result)
 
     def __repr__(self):
-        return " ".join([str(op) for op in self.operators])
+        result = ''
+        for operator, value in self._objects.items():
+            sign = '-' if value < 0 else '+'
+            ops = ''.join(str(op) for op in operator)
+            result += f'{sign} ({abs(value):.06f}) {ops}\n'
+        return result
 
-class FermionStrings:
-    'multiple FermionString objects.'
-    def __init__(self, *args):
-        self.fermion_strings = args
-    
-    def 
-
-def split_op(operators):
+def split_op(val, obj) -> dict:
     'Split the operators in a string and return a list of Fermion objects.'
-    operators = operators.split()
-    return [Fermion(op) for op in operators]
+    obj = tuple(Fermion(v) for v in obj.split())
+    return {op: v for v, op in fermionic_sort(val, obj)}
+
+def fermionic_sort(val, obj):
+    'Sort the operators in a string.'
+    val, obj = num_sort(val, obj)
+    for v, op in dirac_sort(val, obj):
+        yield v, op
+
+def num_sort(val, obj):
+    'Sort the operators by the number of the fermion.'
+    obj_list = list(obj)
+    swapped = False
+    for i, j in pairwise(range(len(obj_list))):
+        if mode(obj_list[i], obj_list[j]) == 'swap':
+            val, obj_list[i], obj_list[j] = -val, obj_list[j], obj_list[i]
+            swapped = True
+    if swapped:
+        return num_sort(val, tuple(obj_list))
+    return val, tuple(obj_list)
+
+def mode(a, b):
+    'Return the mode of sorting.'
+    if a.type == b.type:
+        if a.num > b.num:
+            return 'swap'
+    if b.type == 'creation':
+        return 'dirac'
+    return 'keep'
+
+def dirac_sort(val, obj):
+    'Sort the operators by the commutator relation.'
+    obj_list = list(obj)
+    for i, j in pairwise(range(len(obj_list))):
+        if mode(obj_list[i], obj_list[j]) == 'dirac':
+            if obj_list[i].num == -obj_list[j].num:
+                reduced_op = obj_list[:i] + obj_list[j+1:]
+                yield from dirac_sort(val, tuple(reduced_op))
+            obj_list[i], obj_list[j] = obj_list[j], obj_list[i]
+            yield from dirac_sort(-val, tuple(obj_list))
+            return
+    yield num_sort(val, tuple(obj_list))
 
 if __name__ == "__main__":
-    fermion_list = ['2', '1^', '3', '2^', '1']
-    fermion_objects = [Fermion(fermion) for fermion in fermion_list]
-    print(fermion_objects)
+    fo1 = FermionicOp(1.00, '1 2 2^ 1^')
+    fo2 = FermionicOp(2.00, '1^ 2^ 2 1')
+    fo3 = fo1 + fo2
+    print(fo3)
